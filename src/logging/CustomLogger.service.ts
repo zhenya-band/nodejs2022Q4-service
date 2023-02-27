df9e455bc0e83150d93ffd846778ac432e39ad5b
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2023-02-28 02:58:21 +0300

    feat: add log levels

--- src/logging/CustomLogger.service.ts
+++ src/logging/CustomLogger.service.ts
@@ -7,6 +7,9 @@ export class CustomLogger extends ConsoleLogger {
   private readonly logFile = 'app.log';
   private readonly maxFileSize = Number(process.env.MAX_LOG_FILE_SIZE_KB) * 1024;
   private writeStream: NodeJS.WritableStream;
+  private errorWriteStream: NodeJS.WritableStream;
+
+  private logLevel = Number(process.env.LOG_LEVEL) || 3;
 
   constructor() {
     super();
@@ -20,28 +23,40 @@ export class CustomLogger extends ConsoleLogger {
   }
 
   error(message: any, stack?: string, context?: string) {
-    super.error(message, stack, context);
-    this.writeLogToFile(message);
+    if (this.logLevel >= 1) {
+      super.error(message, stack, context);
+      this.writeLogToFile(message);
+      this.writeErrorLogToFile(message);
+    }
   }
 
   warn(message: string) {
-    super.warn(message);
-    this.writeLogToFile(message);
+    if (this.logLevel >= 2) {
+      super.warn(message);
+      this.writeLogToFile(message);
+    }
   }
 
   debug(message: string) {
-    super.debug(message);
-    this.writeLogToFile(message);
+    if (this.logLevel >= 3) {
+      super.debug(message);
+      this.writeLogToFile(message);
+    }
   }
 
   verbose(message: string) {
-    super.verbose(message);
-    this.writeLogToFile(message);
+    if (this.logLevel >= 4) {
+      super.verbose(message);
+      this.writeLogToFile(message);
+    }
   }
 
   private createWriteStream() {
     const path = join(this.logDir, this.logFile);
+    const errorsPath = join(this.logDir, 'app-errors.log');
+
     this.writeStream = createWriteStream(path, { flags: 'a' });
+    this.errorWriteStream = createWriteStream(errorsPath, { flags: 'a' });
   }
 
   private checkLogDirectoryExists() {
@@ -55,6 +70,13 @@ export class CustomLogger extends ConsoleLogger {
 
     const formattedMessage = `${this.getTimestamp()} ${message}\n`;
     this.writeStream.write(formattedMessage);
+  }
+
+  private writeErrorLogToFile(message: string) {
+    this.checkLogFileSize();
+
+    const formattedMessage = `${this.getTimestamp()} ${message}\n`;
+    this.errorWriteStream.write(formattedMessage);
   }
 
   private checkLogFileSize() {
